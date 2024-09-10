d4bbc71e2eee586ca0a13c82f2779a3132d93a5c
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2024-09-09 20:42:44 -0700

    Update release_servo.py

--- Lab1/release_servo.py
+++ Lab1/release_servo.py
@@ -7,5 +7,6 @@ import pymycobot as mc
 #          or mc = MyCobot("/dev/ttyAMA0", 115200)
 #           windows:
 #              mc = MyCobot("COM3", 115200)
-
+from pymycobot import MyCobot
+mc = MyCobot("COM8 ", 115200)
 mc.release_all_servos()
